7bb3bac2bd541a51a6630eeca3cf7a714127dd67
Fareed <fareed543@gmail.com>
2018-01-10 23:17:00 +0530

    Generate Components using Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './app.materialmodule';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { CareerComponent } from './career/career.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    CareerComponent
   ],
   imports: [
     BrowserModule,
