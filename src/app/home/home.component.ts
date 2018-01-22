6e76c486dd4973d3cded3d58f1d80678d09572ee
Fareed <fareed543@gmail.com>
2018-01-22 11:37:51 +0530

    Misc Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title :string = 'Angular 5 Application';
+  title :string = 'It`s Angular5 Blog';
   constructor() { }
 
   ngOnInit() {
